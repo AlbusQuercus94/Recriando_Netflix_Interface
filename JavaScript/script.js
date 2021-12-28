d430f46ae2713ddb63dee3304e91f0bddf86d7c1
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-12-28 18:30:10 -0300

    Initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        500:{
+            items:3
+        },
+        700:{
+            items:4
+        },
+        850:{
+            items:5
+        },
+        1000:{
+            items:6
+        }
+    }
+})
